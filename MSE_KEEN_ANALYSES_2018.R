2c97e5d353d5a7d7c12406956592af4b85916f6f
whippo <rosspitality@gmail.com>
2018-07-09 18:28:34 -0700

    Created MDS figure and changed 'definitive' to 'instructor'

--- MSE_KEEN_ANALYSES_2018.R
+++ MSE_KEEN_ANALYSES_2018.R
@@ -56,6 +56,7 @@ library(tidyr)
 library(ggplot2)
 library(viridis)
 library(ggpubr)
+library(vegan)
 
 ###################################################################################
 # READ IN AND PREPARE DATA                                                        #
@@ -65,10 +66,10 @@ quaddata <- read.csv("QUAD_Data.csv")
 codes <- read.csv("WA_SP_CODES.csv")
 subs <- read.csv("UPC_SUBS_CODES.csv")
 
-# create column designating 'definitive' vs 'student' data
+# create column designating 'INSTRUCTOR' vs 'student' data
 quaddata <- quaddata %>%
   mutate(COLLECTION_TYPE = ifelse(OBSERVER == "Lowe" | OBSERVER == "Galloway", 
-                                  "DEFINITIVE", "STUDENT"))
+                                  "INSTRUCTOR", "STUDENT"))
 quaddata$COLLECTION_TYPE <- as.factor(quaddata$COLLECTION_TYPE)
 
 # create column of full species names
@@ -94,7 +95,6 @@ UPC <- quaddata %>%
 
 # summarize counts of organisms for quads by group
 quad_count_groups <- QUADS %>%
-  subset(DATA_TYPE == "QUAD") %>%
   group_by(COLLECTION_TYPE, GroupingCode) %>%
   summarise(sum(COUNT))
 names(quad_count_groups)[names(quad_count_groups)=="sum(COUNT)"] <- "TOT_COUNT"
@@ -104,7 +104,32 @@ UPC_count_groups <- UPC %>%
   subset(DATA_TYPE == "UPC") %>%
   group_by(COLLECTION_TYPE, GroupingCode) %>%
   summarise(sum(COUNT)) 
-names(UPC_count_group)[names(UPC_count_groups)=="sum(COUNT)"] <- "TOT_COUNT"
+names(UPC_count_groups)[names(UPC_count_groups)=="sum(COUNT)"] <- "TOT_COUNT"
+
+
+# Create matrix for community composition analysis
+quad_matrix <- QUADS %>%
+  group_by(COLLECTION_TYPE, PLOT, MO_SP_CODE) %>%
+  summarise(sum(COUNT))
+names(quad_matrix)[names(quad_matrix)=="sum(COUNT)"] <- "TOT_COUNT"
+
+KEEN_mat_quad_full <- quad_matrix %>%
+  spread(MO_SP_CODE, TOT_COUNT, fill = 0)
+KEEN_quad_mat_run <- KEEN_mat_quad_full[,c(3:62)]
+
+# MDS of community
+KEEN_quad_mds <- metaMDS(KEEN_quad_mat_run)
+KEEN_quad_mds_points <- KEEN_quad_mds$points
+KEEN_quad_mds_points <- data.frame(KEEN_quad_mds_points)
+plot_data_mds <- data.frame(KEEN_quad_mds_points, KEEN_mat_quad_full[,1:2])
+vec_quad <- envfit(KEEN_quad_mds$points, KEEN_quad_mat_run, perm = 1000)
+vec_quad_df <- as.data.frame(vec_quad$vectors$arrows*sqrt(vec_quad$vectors$r))
+vec_quad_df$species <- rownames(vec_quad_df)
+library(plyr)
+chulls_quad <- ddply(plot_data_mds, .(COLLECTION_TYPE), function(df) df[chull(df$MDS1, df$MDS2), ])
+detach(package:plyr)
+#plot_data_mds$PLOT <- as.factor(plot_data_mds$PLOT)
+
 
 
 ###################################################################################
@@ -123,7 +148,7 @@ COUNT_GROUPS_TYPE_QUAD <- ggplot(quad_count_groups, aes(x = COLLECTION_TYPE, y =
 COUNT_GROUPS_TYPE_QUAD
 
 # Figure of counts of observed of groups by collection type for UPC
-COUNT_GROUPS_TYPE_UPC <- ggplot(UPC_count_group, aes(x = COLLECTION_TYPE, y = TOT_COUNT)) + geom_bar(stat = "identity", aes(fill = GroupingCode)) + 
+COUNT_GROUPS_TYPE_UPC <- ggplot(UPC_count_groups, aes(x = COLLECTION_TYPE, y = TOT_COUNT)) + geom_bar(stat = "identity", aes(fill = GroupingCode)) + 
   theme_minimal() + 
   scale_fill_viridis(discrete=TRUE, option = "plasma") +
   labs(x="", y="Count") +
@@ -146,9 +171,20 @@ annotate_figure(Figure1, bottom = text_grob("Figure 1: Total contribution of tax
 # Visualization of community composition by Students and Instructors from open
 # quadrats
 
+# MDS
+QUAD_MDS <- ggplot(plot_data_mds, aes(x=MDS1, y=MDS2, pch = COLLECTION_TYPE, color = PLOT)) +  
+  theme_minimal() +
+  geom_polygon(data=chulls_quad, aes(x=MDS1, y=MDS2, group=COLLECTION_TYPE), fill=NA, color = "grey") +
+  geom_point(size = 4) +
+  scale_color_viridis_c()
 
+Figure2 <- ggarrange(QUAD_MDS, 
+                     ncol = 1, nrow = 1,
+                     common.legend = FALSE, 
+                     legend = "right")
+annotate_figure(Figure2, bottom = text_grob("Figure 2: MDS of open quadrat communities surveyed by students comparted to surveys \n conducted by instructors.", size = 10))
 
-
+# best size 600x850
 
 #####
 #<<<<<<<<<<<<<<<<<<<<<<<<<<END OF SCRIPT>>>>>>>>>>>>>>>>>>>>>>>>#
